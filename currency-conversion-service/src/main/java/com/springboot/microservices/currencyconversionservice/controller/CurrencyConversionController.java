18db3e5ae09da45d349cad137f7f81489a83c568
nikitha2 <gullapalli.nikitha@gmail.com>
2023-06-29 18:05:33 -0700

    Disable Spring cloud Eureka, Api-gateway, zipkin : Changes to work with Kubernetes

--- currency-conversion-service/src/main/java/com/springboot/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/springboot/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,7 +1,9 @@
 package com.springboot.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import org.slf4j.Logger;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +17,16 @@ public class CurrencyConversionController {
 	@Autowired
 	CurrencyConversionControllerDaoService currencyConversionControllerDaoService;
 	
+	  private Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+	
 	//http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10
 	@GetMapping("/currency-conversion/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
 	public CurrencyConversionEntity findByFromAndToAndQuantityWithRestTemplate(
 			@PathVariable String fromCurrency,
 			@PathVariable String toCurrency,
 			@PathVariable BigDecimal quantity) {
+		
+		logger.info("findByFromAndToAndQuantityWithRestTemplate called with from:{}, to: {}, and quantity: {}",fromCurrency,toCurrency,quantity);
 		
 		return currencyConversionControllerDaoService.findByFromAndToAndQuantityWithRestTemplate(fromCurrency,toCurrency,quantity);
 		
@@ -32,6 +38,8 @@ public class CurrencyConversionController {
 			@PathVariable String toCurrency,
 			@PathVariable BigDecimal quantity) {
 		
+		logger.info("findByFromAndToAndQuantityWithFeign called with from:{}, to: {}, and quantity: {}",fromCurrency,toCurrency,quantity);
+
 		return currencyConversionControllerDaoService.findByFromAndToAndQuantityWithFeign(fromCurrency,toCurrency,quantity);
 		
 	}
